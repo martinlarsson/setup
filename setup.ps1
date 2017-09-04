ad60460754da4e6ecc8e03a15334eca0ff22a5c2
Martin Larsson <martin@mlarsson.se>
2017-09-04 16:30:06 +0200

    first test

---
+++ setup.ps1
@@ -0,0 +1,36 @@
+# Chocolatey
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Dev. tools
+choco install git
+choco install putty
+choco install jdk8
+choco install php
+choco install mysql
+choco install nodejs
+choco install sql-server-management-studio
+choco install visualstudio2017enterprise
+choco install jetbrainstoolbox
+choco install miktex
+choco install texmaker
+choco install staruml2
+choco install python
+pip install pelican
+
+## Atom + packages
+choco install atom
+apm install file-icons, minimap, w3c-validation, browser-plus, open-in-browser, atom-beautify, language-powershell, language-latex, pdf-view, latex-completions, monokai
+
+# Browsers
+choco install googlechrome
+choco install firefox
+
+# Misc.
+choco install adobereader
+choco install 7zip
+choco install vlc
+choco install filezilla
+choco install javaruntime
+choco install spotify
+choco install dropbox
+choco install googledrive
